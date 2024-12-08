be90b1f6ec8bc71832df47030f6e9a3308630573
Daniel R <nzdanron@gmail.com>
2024-12-08 17:17:20 +1300

    feat: add animation to buttons

--- src/views/icons/PlayIcon.js
+++ src/views/icons/PlayIcon.js
@@ -12,7 +12,7 @@ const PlayIcon = {
         height: `${size}px`,
         viewBox: '0 -960 960 960',
         width: `${size}px`,
-        class: 'fill-secondary',
+        class: 'fill-secondary hover:opacity-50 ease-in-out duration-300 cursor-pointer',
       },
       m('path', { d: 'M320-202v-560l440 280-440 280Zm66.67-280Zm0 158.67L636-482 386.67-640.67v317.34Z' })
     );
