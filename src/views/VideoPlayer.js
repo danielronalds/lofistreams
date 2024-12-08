f8130c9f254452b94d8854ab902a38bfdcb54f13
Daniel R <nzdanron@gmail.com>
2024-12-08 16:49:53 +1300

    feat: VideoPlayer always takes up fullscreen

--- src/views/VideoPlayer.js
+++ src/views/VideoPlayer.js
@@ -8,14 +8,16 @@ const VideoPlayer = {
   },
   view: () => {
     const videoId = AppModel.getCurrentStreamId();
-    return m('div', [
+    return m('div', {
+      class: 'w-screen h-screen bg-black absolute top-0 left-0 overflow-hidden'
+    }, [
       m('iframe', {
         id: 'iframe',
         src:
           'https://www.youtube.com/embed/' +
           videoId +
           '?si=bZ9hFpMbL1xoD7vQ&controls=0&autoplay=0&enablejsapi=1&modestbranding=1&disablekb=1&playsinline=1&iv_load_policy=3',
-        class: 'pointer-events-none h-[110vh] w-[116vw] top-[-10vh] left-[-16vw] absolute z-0 select-none',
+        class: 'pointer-events-none h-screen w-screen top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] absolute z-0 select-none',
         frameborder: '0',
         referrerpolicy: 'strict-origin-when-cross-origin',
       }),
